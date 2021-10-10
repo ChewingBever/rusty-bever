8e433c32501046da330db5f1febe889ea81bca99
Jef Roosens <roosensjef@gmail.com>
2021-10-10 09:28:21 +0200

    Replaced web folder

---
+++ web/src/main.ts
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import { makeServer } from "./server"
+
+if (process.env.NODE_ENV === "development") {
+    makeServer()
+}
+
+createApp(App).mount('#app')
